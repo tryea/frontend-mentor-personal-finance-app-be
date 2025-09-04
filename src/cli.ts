5c89daa05a855d7cd56e3bc1a3b9db6e407d5d15
Ersapta Aristo <aristoersapta@gmail.com>
2025-09-04 20:03:39 +0700

    reset everything

--- src/cli.ts
+++
@@ -1,8 +0,0 @@
-import { CommandFactory } from 'nest-commander';
-import { SeederModule } from './infrastructure/database/seeder.module';
-
-async function bootstrap() {
-  await CommandFactory.run(SeederModule, ['warn', 'error']);
-}
-
-bootstrap();
